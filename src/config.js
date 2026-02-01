5e6db2ba4e88474198ad9b7a6632ce2a2b7c07c1
N-H-fujita <86991819+N-H-fujita@users.noreply.github.com>
2026-02-01 14:07:07 +0900

    chore: config.js に暫定パスと社員IDコメントを追加 (TODO付き)

--- src/config.js
+++ src/config.js
@@ -1,6 +1,13 @@
+// TODO: 将来的には data/employees.json から取得
 const EMPLOYEE_IDS = [
   "a001",
   "a002",
 ];
 
-module.exports = { EMPLOYEE_IDS };
+// TODO: serverBasePath は現状固定。将来的には .env から取得
+const serverBasePath = "//server/csv"; //コピー元
+
+// TODO: localDir は現状固定。将来的には .env から取得
+const localDir = "./local_csv";        // コピー先
+
+module.exports = { EMPLOYEE_IDS, serverBasePath, localDir };
