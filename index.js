85ec105a3cf8a0005bfe0620acc85ea89103121e
N-H-fujita <86991819+N-H-fujita@users.noreply.github.com>
2026-02-01 11:37:29 +0900

    feat: CSVファイル名生成のロジックを追加

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const { parseArgs } = require('./src/args');
+const { EMPLOYEE_IDS } = require('./src/config');
+const { generateCsvFilenames } = require('./src/fileNames');
 
 function main() {
+
+  // --- 年・月を取得 ---
   const { year, month } = parseArgs();
-  console.log((`Target: ${year}-${String(month).padStart(2, '0')}`));
+
+  // --- ファイル名配列作成 ---
+  const csvFileNames = generateCsvFilenames(year, month, EMPLOYEE_IDS)
+
+  console.log(csvFileNames);
 }
 
 main();
